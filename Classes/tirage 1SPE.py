38d16e9bc87f936b25b7995e0bf36c6a07a67424
Rweye6 <teach.rweye@gmail.com>
2025-12-10 13:58:00 +0100

    groupes 1spe mis à jour

--- Classes/tirage 1SPE.py
+++ Classes/tirage 1SPE.py
@@ -3,22 +3,35 @@ import matplotlib.pyplot as plt
 import random
 import csv
 import pandas as pd
+import os
 
+BASE = os.path.abspath(os.path.dirname(__file__))
+df = pd.read_csv(os.path.join(BASE, "1SPE2.csv"), delimiter=";")
+# df = pd.read_csv("1SPE2.csv", delimiter =";")
 
-df = pd.read_csv("1SPE2.csv", delimiter =";")
-
-#Groupes 1 et 2
 etu = list(df["Prénom"])
-#groupe = list(df["Part"])
-ligne = list()
-
 random.shuffle(etu)
-ligne.append("Les binomes sont : ")
-for i in range(0,len(etu)) :
-    if int(i/2) == i/2 and i != len(etu)-1 : ligne.append(etu[i] +" et " + etu[i+1])
-    elif int(i/2) == i/2 and i == len(etu)-1 : ligne.append(etu[i])
-    else : next
 
-with open('Groupes TP 1SPE2.txt', 'w') as fichier:
-    fichier.write('\n'.join(map(str, ligne)))
+# Création des binômes
+binomes = []
+for i in range(0, len(etu), 2):
+    if i+1 < len(etu):
+        binomes.append(f"{etu[i]} et {etu[i+1]}")
+    else:
+        binomes.append(f"{etu[i]}")
 
+# Fichier TEX des binômes
+binomes_tex_path = os.path.join(BASE, "binomes1.tex")
+
+with open(binomes_tex_path, "w", encoding="utf-8") as f:
+    for p in binomes:
+        f.write(f"\\item {p}\n")
+
+print("✔ Fichier binomes.tex généré.")
+
+# Compilation
+texfile = os.path.join(BASE, "Groupes_TP_1SPE2.tex")
+cmd = f'pdflatex -interaction=nonstopmode -output-directory="{BASE}" "{texfile}"'
+os.system(cmd)
+
+print("🎉 PDF généré :", BASE)
