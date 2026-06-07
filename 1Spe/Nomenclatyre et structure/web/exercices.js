ddccf365b62f6ff8a8d1ae373b5c899d209565f2
Rweye6 <teach.rweye@gmail.com>
2026-06-07 18:32:54 +0200

    exercices.js : correction de la fonction qui affiche les exercices

--- 1Spe/Nomenclatyre et structure/web/exercices.js
+++ 1Spe/Nomenclatyre et structure/web/exercices.js
@@ -1,3 +1,55 @@
 const reponse1 = document.querySelector("#rep1")
+const val1 = document.querySelector("#val1")
+const result1 = document.querySelector("#result1")
 
-console.log(reponse1)
+val1.addEventListener("click", () => {
+     if (reponse1.value =="1,1-diméthyléthanol") {
+        result1.textContent = "Bonne réponse !"
+    } else {
+        result1.textContent = "Mauvaise réponse !"
+    }
+})
+
+const reponse2 = document.querySelector("#rep2")
+const val2 = document.querySelector("#val2")
+const result2 = document.querySelector("#result2")
+
+val2.addEventListener("click", () => {
+     if (reponse2 =="pentanal") {
+        result2.textContent = "Bonne réponse !"
+    } else {
+        result2.textContent = "Mauvaise réponse !"
+    }
+})
+
+const reponse3 = document.querySelector("#rep3")
+const val3 = document.querySelector("#val3")
+const result3 = document.querySelector("#result3")
+
+val3.addEventListener("click", () => {
+     if (reponse3 =="acide octanoïque") {
+        result3.textContent = "Bonne réponse !"
+    } else {
+        result3.textContent = "Mauvaise réponse !"
+    }
+})
+
+const reponse4 = document.querySelector("#rep4")
+const val4 = document.querySelector("#val4")
+const result4 = document.querySelector("#result4")
+
+val4.addEventListener("click", () => {
+    if (reponse4.value =="butan-2,3-diol") {
+        result4.textContent = "Bonne réponse !"
+    } else {
+        result4.textContent = "Mauvaise réponse !"
+    }
+})
+
+val4.addEventListener("click", () => {
+     if (reponse4.value =="butan-2,3-diol") {
+        result4.textContent = "Bonne réponse !"
+    } else {
+        result4.textContent = "Mauvaise réponse !"
+    }
+})
